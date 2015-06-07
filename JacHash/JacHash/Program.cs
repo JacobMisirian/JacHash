c8df38c056263fe3e5f5de5503e7715acd890cf0
Jacob Misirian <misirianrule@gmail.com>
2015-06-06 19:58:54 -0500

    INCOMPLETE CHANGES

--- JacHash/JacHash/Program.cs
+++ JacHash/JacHash/Program.cs
@@ -8,10 +8,21 @@ namespace JacHash
         {
             while (true)
             {
-                Console.WriteLine("Enter in a string: ");
-                string theString = Console.ReadLine();
-                JHash hash = new JHash(theString);
-                Console.WriteLine(hash.Generate());
+                Console.Write("Salted or unsalted hash: s/u ");
+                if (Console.ReadLine() == "s")
+                {
+                    Console.Write("Enter in the string: ");
+                    string text = new JHash(Console.ReadLine()).Generate();
+                    Console.Write("Enter in the salt: ");
+                    string salt = new JHash(Console.ReadLine()).Generate();
+                    Console.WriteLine("Result is: " + new JHash(text + salt).Generate());
+                }
+                else
+                {
+                    Console.WriteLine("Enter in a string: ");
+                    string theString = Console.ReadLine();
+                    Console.WriteLine("Result is: " + new JHash(theString).Generate());
+                }
             }
         }
     }
