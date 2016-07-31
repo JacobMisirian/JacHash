3529dd9811618937f84e4e6a4767436c6801b071
Jacob Misirian <misirianrule@gmail.com>
2016-07-30 23:05:56 -0500

    Updated switches

--- src/JacHash/Program.cs
+++ src/JacHash/Program.cs
@@ -12,11 +12,20 @@ namespace JacHash
         static void Main(string[] args)
         {
             JacHash hash = new JacHash();
-            Console.WriteLine(hash.Hash(ASCIIEncoding.ASCII.GetBytes(args[0])));
+            switch (args[0])
+            {
+                case "-s":
+                case "--string":
+                    Console.WriteLine(hash.Hash(ASCIIEncoding.ASCII.GetBytes(args[1])));
+                    break;
+                case "-r":
+                case "--repl":
+                    while (true)
+                        Console.WriteLine(hash.Hash(ASCIIEncoding.ASCII.GetBytes(Console.ReadLine())));
+            }
          /*   hash.TestBrute("0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz", 8);
             while (true)
             {
-                Console.WriteLine(hash.Hash(ASCIIEncoding.ASCII.GetBytes(Console.ReadLine())));
             }
             */
         }
